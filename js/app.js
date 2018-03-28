a1e8d8c3cb5e643029828b57ab9db10d61d8f5bb
Ted Jacobs <doldrone@bellsouth.net>
2018-03-27 21:37:39 -0400

    Trying to freaking upload my project

---
+++ js/app.js
@@ -0,0 +1,52 @@
+
+// Open the modal
+
+function openModal() {
+  document.getElementById('myModal').style.display = "flex";
+}
+
+
+// Close the Modal
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+
+  function newFunction() {
+    return "none";
+  }
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+// Previous/Next controls
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt
+}
+
